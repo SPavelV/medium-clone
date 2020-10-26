395bf8f99ef800699011b8aa8aa29533f989c38f
PavelSolovev <pashius2010@gmail.com>
2020-10-26 15:55:23 +0400

    nav

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "routes";
+import { TopBar } from "components/topBar";
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to hooks</h3>
       <Router>
-        <Routes/>
+        <TopBar />
+        <Routes />
       </Router>
     </div>
   );
