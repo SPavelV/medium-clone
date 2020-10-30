bda791cf81bbe4b188641889729c716c54e0a259
PavelSolovev <pashius2010@gmail.com>
2020-10-30 09:03:47 +0400

    add context to current user

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "routes";
 import { TopBar } from "components/topBar";
+import { CurrentUserProvider } from "contexts/currentUser";
 
 const App = () => {
   return (
-    <div>
+    <CurrentUserProvider>
       <Router>
         <TopBar />
         <Routes />
       </Router>
-    </div>
+    </CurrentUserProvider>
   );
 };
 
@@ -19,5 +20,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
