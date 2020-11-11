3b4dbf5429e81dbbbbf67bccaa9edfb0dd423270
Pavel Solovev <pashius2010@gmail.com>
2020-11-12 02:36:17 +0400

    get current user

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "routes";
 import { TopBar } from "components/topBar";
 import { CurrentUserProvider } from "contexts/currentUser";
+import CurrentUserChecker from "components/currentUserChecker";
 
 const App = () => {
   return (
     <CurrentUserProvider>
-      <Router>
-        <TopBar />
-        <Routes />
-      </Router>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
     </CurrentUserProvider>
   );
 };
@@ -20,5 +23,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
